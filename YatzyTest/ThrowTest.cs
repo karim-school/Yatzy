7a2e27c614d976164c4d438e8b87832f3d2d9387
Karim <72211@edu.sde.dk>
2025-08-19 09:58:09 +0200

    feat: add unit tests for checking expected values

--- YatzyTest/ThrowTest.cs
+++ YatzyTest/ThrowTest.cs
@@ -16,6 +16,17 @@ public class ThrowTest
     }
     
     [Theory]
+    [InlineData(15, 1, 2, 3, 4, 5)]
+    [InlineData(15, 1, 2, 3, 4, 5, 6)]
+    [InlineData(15, 5, 4, 3, 2, 1)]
+    [InlineData(15, 1, 3, 2, 4, 6, 5)]
+    public void TestThatSmallStraightValuesAreCorrect(int expected, params int[] values)
+    {
+        var combinations = Throw.SMALL_STRAIGHT.GetValid(values.Select(i => Die.Fixed((uint)i)).ToArray());
+        Assert.Equal(1, combinations.Count(combination => Throw.SMALL_STRAIGHT.CountDice(combination) == expected));
+    }
+    
+    [Theory]
     [InlineData(2, 3, 4, 5, 6)]
     [InlineData(1, 2, 3, 4, 5, 6)]
     [InlineData(6, 5, 4, 3, 2)]
@@ -24,6 +35,17 @@ public class ThrowTest
     {
         var dice = values.Select(i => Die.Fixed((uint)i)).ToArray();
         Assert.True(Throw.BIG_STRAIGHT.IsValid(dice) && Throw.BIG_STRAIGHT.GetValid(dice).Count != 0);
+    }
+    
+    [Theory]
+    [InlineData(20, 2, 3, 4, 5, 6)]
+    [InlineData(20, 1, 2, 3, 4, 5, 6)]
+    [InlineData(20, 6, 5, 4, 3, 2)]
+    [InlineData(20, 1, 3, 2, 4, 6, 5)]
+    public void TestThatBigStraightValuesAreCorrect(int expected, params int[] values)
+    {
+        var combinations = Throw.BIG_STRAIGHT.GetValid(values.Select(i => Die.Fixed((uint)i)).ToArray());
+        Assert.Equal(1, combinations.Count(combination => Throw.BIG_STRAIGHT.CountDice(combination) == expected));
     }
     
     [Theory]
@@ -37,6 +59,16 @@ public class ThrowTest
     }
     
     [Theory]
+    [InlineData(21, 1, 2, 3, 4, 5, 6)]
+    [InlineData(21, 6, 5, 4, 3, 2, 1)]
+    [InlineData(21, 1, 3, 2, 4, 6, 5)]
+    public void TestThatRoyalStraightValuesAreCorrect(int expected, params int[] values)
+    {
+        var combinations = Throw.ROYAL_STRAIGHT.GetValid(values.Select(i => Die.Fixed((uint)i)).ToArray());
+        Assert.Equal(1, combinations.Count(combination => Throw.ROYAL_STRAIGHT.CountDice(combination) == expected));
+    }
+    
+    [Theory]
     [InlineData(6, 6, 6, 5, 5)]
     [InlineData(6, 6, 5, 5, 5)]
     [InlineData(6, 6, 6, 5, 5, 5)]
@@ -46,6 +78,16 @@ public class ThrowTest
     {
         var dice = values.Select(i => Die.Fixed((uint)i)).ToArray();
         Assert.True(Throw.FULL_HOUSE.IsValid(dice) && Throw.FULL_HOUSE.GetValid(dice).Count != 0);
+    }
+
+    [Theory]
+    [InlineData(28, 6, 6, 6, 5, 5)]
+    [InlineData(27, 6, 6, 5, 5, 5)]
+    [InlineData(18, 4, 3, 3, 4, 4)]
+    public void TestThatFullHouseValuesAreCorrect(int expected, params int[] values)
+    {
+        var combinations = Throw.FULL_HOUSE.GetValid(values.Select(i => Die.Fixed((uint)i)).ToArray());
+        Assert.Equal(1, combinations.Count(combination => Throw.FULL_HOUSE.CountDice(combination) == expected));
     }
     
     [Theory]
